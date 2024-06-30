2687ba94ebb9757f1db35859a761a1497d9c1761
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-06-30 23:28:48 +0200

    Added first database structure

---
+++ models/DBProcessQueue.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('DBProcessQueue', {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		guildId: {
+			type: DataTypes.STRING,
+			allowNullValue: false,
+		},
+		task: DataTypes.STRING,
+		priority: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+		},
+		filters: DataTypes.STRING,
+		additions: DataTypes.STRING,
+		date: {
+			type: DataTypes.DATE,
+		},
+		beingExecuted: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: false,
+		}
+	}, {
+		indexes: [
+			{
+				unique: false,
+				fields: ['beingExecuted', 'date', 'priority']
+			}
+		]
+	});
+};
