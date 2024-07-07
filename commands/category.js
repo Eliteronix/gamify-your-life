0c6017759e1288bdbb962433168eb04acf437f89
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-07-07 19:48:13 +0200

    Added first command

---
+++ commands/category.js
@@ -0,0 +1,133 @@
+const { DBCategories } = require('../dbObjects');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	name: 'category',
+	data: new SlashCommandBuilder()
+		.setName('category')
+		.setNameLocalizations({
+			'en-GB': 'category',
+			'en-US': 'category',
+		})
+		.setDescription('Create and delete task categories')
+		.setDescriptionLocalizations({
+			'en-GB': 'Create and delete task categories',
+			'en-US': 'Create and delete task categories',
+		})
+		.setDMPermission(false)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('create')
+				.setNameLocalizations({
+					'en-GB': 'create',
+					'en-US': 'create',
+				})
+				.setDescription('Create a new task category')
+				.setDescriptionLocalizations({
+					'en-GB': 'Create a new task category',
+					'en-US': 'Create a new task category',
+				})
+				.addStringOption(option =>
+					option
+						.setName('name')
+						.setNameLocalizations({
+							'en-GB': 'name',
+							'en-US': 'name',
+						})
+						.setDescription('The name of the task category')
+						.setDescriptionLocalizations({
+							'en-GB': 'The name of the task category',
+							'en-US': 'The name of the task category',
+						})
+						.setRequired(true)
+				)
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('delete')
+				.setNameLocalizations({
+					'en-GB': 'delete',
+					'en-US': 'delete',
+				})
+				.setDescription('Delete a task category')
+				.setDescriptionLocalizations({
+					'en-GB': 'Delete a task category',
+					'en-US': 'Delete a task category',
+				})
+				.addStringOption(option =>
+					option
+						.setName('name')
+						.setNameLocalizations({
+							'en-GB': 'name',
+							'en-US': 'name',
+						})
+						.setDescription('The name of the task category')
+						.setDescriptionLocalizations({
+							'en-GB': 'The name of the task category',
+							'en-US': 'The name of the task category',
+						})
+						.setRequired(true)
+						.setAutocomplete(true)
+				)
+		),
+	async autocomplete(interaction) {
+		const focusedValue = interaction.options.getFocused();
+
+		const categories = await DBCategories.findAll({
+			attributes: ['name'],
+			where: {
+				guildId: interaction.guild.id,
+			},
+			group: ['name'],
+		});
+
+		let autocompleteCategories = [];
+
+		categories.forEach(category => {
+			autocompleteCategories.push({
+				name: category.name,
+				creatorId: category.name,
+			});
+		});
+
+		let filtered = mappools.filter(choice => choice.name.toLowerCase().includes(focusedValue.toLowerCase()));
+
+		filtered = filtered.slice(0, 25);
+
+		if (filtered.length === 0) {
+			try {
+				await interaction.respond([{
+					name: 'No categories found | Create a category first',
+					value: 'No categories found | Create a category first',
+				}]);
+			} catch (error) {
+				if (error.message !== 'Unknown interaction' && error.message !== 'The reply to this interaction has already been sent or deferred.') {
+					console.error(error);
+				}
+			}
+			return;
+		}
+
+		try {
+			await interaction.respond(
+				filtered.map(choice => ({ name: choice.name, value: choice.name })),
+			);
+
+			gotResponse = true;
+		} catch (error) {
+			if (error.message !== 'Unknown interaction' && error.message !== 'The reply to this interaction has already been sent or deferred.') {
+				console.error(error);
+			}
+		}
+	},
+	async execute(interaction) {
+		try {
+			await interaction.deferReply({ ephemeral: true });
+		} catch (error) {
+			if (error.message !== 'Unknown interaction') {
+				console.error(error);
+			}
+			return;
+		}
+	},
+};
--- interactionCreate.js
+++ interactionCreate.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const { PermissionsBitField } = require('discord.js');
 
 module.exports = async function (client, bancho, interaction) {
 	process.send(`discorduser ${interaction.user.id}}`);
@@ -21,45 +20,7 @@ module.exports = async function (client, bancho, interaction) {
 	}
 
 	if (interaction.isCommand()) {
-		//Set the command and check for possible uses of aliases
-		let command = client.commands.get(interaction.commandName)
-			|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(interaction.commandName));
-
-		//Check permissions of the bot
-		if (interaction.guildId) {
-			let member = interaction.guild.members.cache.get(client.user.id);
-
-			while (!member) {
-				try {
-					member = await interaction.guild.members.fetch({ user: [client.user.id], time: 300000 })
-						.catch((err) => {
-							throw new Error(err);
-						});
-
-					member = member.first();
-				} catch (e) {
-					if (e.message !== 'Members didn\'t arrive in time.') {
-						console.error('interactionCreate.js | Check bot permissions', e);
-						return;
-					}
-				}
-			}
-
-			const botPermissions = interaction.channel.permissionsFor(member);
-			if (!botPermissions || !botPermissions.has(PermissionsBitField.Flags.ViewChannel)) {
-				//The bot can't possibly answer the message
-				return await interaction.reply({ content: 'I can\'t view this channel.', ephemeral: true });
-			}
-
-			//Check the command permissions
-			if (command.botPermissions) {
-				if (!botPermissions.has(command.botPermissions)) {
-					return await interaction.reply({ content: `I need the ${command.botPermissionsTranslated} permission to do this!`, ephemeral: true });
-				}
-			}
-		}
-
-		command.execute(null, [], interaction, [client, bancho]);
+		command.execute(interaction);
 	} else if (interaction.isAutocomplete()) {
 		const command = interaction.client.commands.get(interaction.commandName);
 
