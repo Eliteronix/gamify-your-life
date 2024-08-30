278e029a58e11f5bfdea1bcb3c532ae426269380
Marvin Zimmermann <64707347+Eliteronix@users.noreply.github.com>
2024-08-30 12:06:42 +0200

    Added reopen date logic

--- commands/done.js
+++ commands/done.js
@@ -1,4 +1,4 @@
-const { DBTasks } = require('../dbObjects');
+const { DBTasks, DBGuildSettings } = require('../dbObjects');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { updateGuildDisplay } = require('../utils');
 
@@ -88,6 +88,42 @@ module.exports = {
 
 		task.done = true;
 		task.dateLastDone = new Date();
+
+		task.dateReopen = new Date();
+
+		if (task.resetEveryHours) {
+			task.dateReopen.setHours(task.dateReopen.getHours() + task.resetEveryHours);
+		} else if (task.resetEveryDays) {
+			let guildSettings = await DBGuildSettings.findOne({
+				attributes: ['dailyResetTime'],
+				where: {
+					guildId: interaction.guild.id,
+				},
+			});
+
+			if (!guildSettings) {
+				guildSettings = await DBGuildSettings.create({
+					guildId: interaction.guild.id,
+					dailyResetTime: 5,
+				});
+			}
+
+			task.dateReopen = new Date();
+
+			task.dateReopen.setMinutes(0);
+			task.dateReopen.setSeconds(0);
+			task.dateReopen.setMilliseconds(0);
+
+			task.dateReopen.setUTCHours(guildSettings.dailyResetTime);
+
+			// If the reset time is in the future, set it to the previous day
+			if (task.dateReopen > new Date()) {
+				task.dateReopen.setDate(task.dateReopen.getDate() - 1);
+			}
+
+			task.dateReopen.setDate(task.dateReopen.getDate() + task.resetEveryDays);
+		}
+
 		await task.save();
 
 		await interaction.editReply(`Task \`${taskName}\` marked as done`);
